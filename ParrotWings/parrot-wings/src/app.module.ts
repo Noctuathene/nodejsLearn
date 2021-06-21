c45aca2ba82e90c3e61511bc002436846fab9982
Roman Gilyazov <roman.gilyazov@sharp-dev.net>
2021-06-21 20:17:01 +0500

    add db config

--- ParrotWings/parrot-wings/src/app.module.ts
+++ ParrotWings/parrot-wings/src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { TransactionsModule } from './transactions/transactions.module'
+import { TransactionsModule } from './transactions/transactions.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '2306',
-    database: 'pw',
-    entities: [],
-    synchronize: true,
-    autoLoadEntities: true,
-  }), UsersModule, TransactionsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.host,
+      port: parseInt(process.env.port),
+      username: process.env.username,
+      password: process.env.password,
+      database: process.env.database,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+    UsersModule,
+    TransactionsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
